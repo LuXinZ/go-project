5b1d07b4bba19f4deeb8626361bab882984dd95c
ll <zhangluxin1995@gmail.com>
2022-12-05 21:51:05 +0800

    c5

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -4,12 +4,37 @@ import (
 	"fmt"
 	"net/http"
 	"net/internal/data"
+	"net/internal/validator"
 	"time"
 )
 
 // post /v1/movies
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	movice := &data.Movie{
+		CreatedAt: time.Time{},
+		Title:     input.Title,
+		Year:      input.Year,
+		Runtime:   input.Runtime,
+		Genres:    input.Genres,
+	}
+	v := validator.New()
+
+	if data.ValidateMovie(v, movice); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	fmt.Fprintf(w, "%+v\n", input)
 }
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 
